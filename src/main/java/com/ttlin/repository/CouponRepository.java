50d143a51890a4293aa207f5154d40ea4fc6b82c
ttlin <871945871@qq.com>
2021-02-26 20:47:41 +0800

    coupon查询接口

---
+++ src/main/java/com/ttlin/repository/CouponRepository.java
@@ -0,0 +1,25 @@
+package com.ttlin.repository;
+
+import com.ttlin.pojo.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    @Query("select c from Coupon c\n" +
+            "join c.categoryList ca\n" +
+            "join Activity a on a.id = c.activityId\n" +
+            "where ca.id = :cid\n" +
+            "and a.startTime < :now\n" +
+            "and a.endTime > :now\n")
+    List<Coupon> findByCategory(Long cid, Date now);
+
+    @Query("select c from Coupon c\n" +
+            "join Activity a on c.activityId = a.id\n" +
+            "where c.wholeStore = :isWholeStore\n" +
+            "and a.startTime < :now\n" +
+            "and a.endTime > :now\n" )
+    List<Coupon> findByWholeStore(Boolean isWholeStore, Date now);
+}
---
+++ src/main/java/com/ttlin/service/CouponService.java
@@ -0,0 +1,26 @@
+package com.ttlin.service;
+
+import com.ttlin.pojo.entity.Coupon;
+import com.ttlin.repository.CouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    public List<Coupon> getByCategory(Long cid) {
+        Date now = new Date();
+        return couponRepository.findByCategory(cid, now);
+    }
+
+    public List<Coupon> getWholeStoreCoupons() {
+        Date now = new Date();
+        return couponRepository.findByWholeStore(true, now);
+    }
+}
